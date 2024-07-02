c301634ed29764ce73fb5642ea4527148f45235d
ThienNT <Nguyenthien11082005@gmail.com>
2024-07-02 15:17:42 +0700

    update project

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udaigram-titi-11080001.us-east-1.elasticbeanstalk.com//api/v0'
+  apiHost: 'http://udaigram-titi-11080001.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
